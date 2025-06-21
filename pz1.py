bc026ad43cf1f020af2a35bd82c42219e94892d7
Dima <123866741+dima7632@users.noreply.github.com>
2025-06-21 14:51:11 +0300

    Create pz1.py

---
+++ pz1.py
@@ -0,0 +1,10 @@
+// завдання 1
+print("Hello, Python!")
+//завдання 2
+name = input("Як тебе звати? ")
+print("Привіт, " + name + "!")
+// завдання 3
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+sum = num1 + num2
+print("Сума чисел:", sum)
